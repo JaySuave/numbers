2a64c696310e923e57aaa4b2954c5fcfd37215f8
JaySuave <up202004823@up.pt>
2021-11-24 18:06:59 +0000

    mockito implementado e coverage total

--- src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -3,12 +3,14 @@ package com.aor.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class ListAggregatorTest {
     private List<Integer> list;
+
     @Test
     public void sum() {
         ListAggregator aggregator = new ListAggregator();
@@ -70,5 +72,10 @@ public class ListAggregatorTest {
         StubListDeduplicator deduplicator = new StubListDeduplicator();
         int distinct = aggregator.distinct(list, deduplicator);
         Assertions.assertEquals(3, distinct);
+    }
+    @Test
+    public void distinct_mockito(){
+        GenericListDeduplicator deduplicator = Mockito.mock(GenericListDeduplicator.class);
+        Mockito.when(deduplicator.deduplicate(Mockito.anyList())).thenReturn(Arrays.asList(1,2,4));
     }
 }
--- src/test/java/com/aor/numbers/ListDeduplicatorTest.java
+++ src/test/java/com/aor/numbers/ListDeduplicatorTest.java
@@ -3,6 +3,7 @@ package com.aor.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,5 +27,10 @@ public class ListDeduplicatorTest {
         ListDeduplicator deduplicator = new ListDeduplicator(sorter);
         List<Integer> distinct = deduplicator.deduplicate(list);
         Assertions.assertEquals(expected, distinct);
+    }
+    @Test
+    public void deduplicate_mockito(){
+        GenericListSorter sorter = Mockito.mock(GenericListSorter.class);
+        Mockito.when(sorter.sort(Mockito.anyList())).thenReturn(Arrays.asList(1,2,4,5));
     }
 }
